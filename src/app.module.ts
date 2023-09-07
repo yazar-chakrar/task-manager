9534d3bdb88c2e3f3d782b2f36fb89e9acaa449b
Mohammed El Amin CHAKRAR <hm_chakrar@esi.dz>
2023-09-07 10:00:15 +0100

    FEATURE:LOGGING(WINSTON)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from "@nestjs/common";
+import { Module, MiddlewareConsumer } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TasksModule } from "./tasks/tasks.module";
 import { typeOrmConfig } from "./config/typeorm.config";
+import { RequestLoggerMiddleware } from "./middlewares/request-logger.middleware";
 
 @Module({
 	imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
 	controllers: [],
 	providers: [],
 })
-export class AppModule {}
+export class AppModule {
+	// add a middleware on all routes
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(RequestLoggerMiddleware).forRoutes("*");
+	}
+}
