2cba72f530ccbf07afff69c499e82503aa692fb3
Mohammed El Amin CHAKRAR <hm_chakrar@esi.dz>
2023-09-07 11:49:30 +0100

    UPDATE:TASK:FILTER

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -13,10 +13,21 @@ export class TaskRepository extends Repository<Task> {
 
 	async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
 		const { status, search } = filterDto;
+		console.log(filterDto);
 		const query = this.createQueryBuilder("task");
 
+		if (status) {
+			query.andWhere("task.status = :status", { status });
+		}
+		if (search) {
+			query.andWhere(
+				"((task.title LIKE :search) OR (task.description LIKE :search))",
+				{
+					search: `%${search}%`,
+				},
+			);
+		}
 		const tasks = await query.getMany();
-
 		return tasks;
 	}
 
