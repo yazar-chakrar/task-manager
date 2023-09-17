75821992a65163e6738c5049ff8a18e86e123508
Mohammed El Amin CHAKRAR <hm_chakrar@esi.dz>
2023-09-17 11:36:01 +0100

    FEATURE:JWT-AUTH

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,22 @@
 import { Body, Controller, Post, ValidationPipe } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
-import { User } from "./user.entity";
 
 @Controller("auth")
 export class AuthController {
 	constructor(private authService: AuthService) {}
 
 	@Post("/signup")
-	async signUp(
+	signUp(
 		@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
 	): Promise<void> {
 		return this.authService.signUp(authCredentialsDto);
 	}
 
 	@Post("/signin")
-	async signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+	signIn(
+		@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+	): Promise<{ accesToken: string }> {
 		return this.authService.signIn(authCredentialsDto);
 	}
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,23 @@ import { AuthService } from "./auth.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./user.entity";
 import { UserRepository } from "./user.repository";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { JwtStrategy } from "./jwt/jwt.strategy";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([User])],
+	imports: [
+		PassportModule.register({ defaultStrategy: "jwt" }),
+		JwtModule.register({
+			secret: "topSecretYazar",
+			signOptions: {
+				expiresIn: 3600,
+			},
+		}),
+		TypeOrmModule.forFeature([User]),
+	],
 	controllers: [AuthController],
-	providers: [AuthService, UserRepository],
+	providers: [JwtStrategy, AuthService, UserRepository],
+	exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
