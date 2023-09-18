aaf274dd919159e551ee0bc9909a0367876fa492
Mohammed El Amin CHAKRAR <hm_chakrar@esi.dz>
2023-09-18 15:52:42 +0100

    FEATURE:HTTPheaders(HELMET)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
 import { WinstonModule } from "nest-winston";
-import { winstonModuleConfig } from "./config/logs/winstonModule.config";
+import {
+	FastifyAdapter,
+	NestFastifyApplication,
+} from "@nestjs/platform-fastify";
+import { AppModule } from "./app.module";
+import { winstonModuleConfig } from "./config/logging/winstonModule.config";
+import helmet from "@fastify/helmet";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, {
-		logger: WinstonModule.createLogger(winstonModuleConfig),
-	});
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter(),
+		{
+			logger: WinstonModule.createLogger(winstonModuleConfig),
+		},
+	);
+	app.register(helmet);
 	await app.listen(3000);
 }
 bootstrap();
