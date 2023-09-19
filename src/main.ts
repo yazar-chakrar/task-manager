319955ba91fa98bab390b04a41522522e81d0e6e
Mohammed El Amin CHAKRAR <hm_chakrar@esi.dz>
2023-09-19 09:42:06 +0100

    FEATURE:CSRF-FASTIFY

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { WinstonModule } from "nest-winston";
+import helmet from "@fastify/helmet";
 import {
 	FastifyAdapter,
 	NestFastifyApplication,
 } from "@nestjs/platform-fastify";
+import fastifyCsrfProtection from "@fastify/csrf-protection";
 import { AppModule } from "./app.module";
 import { winstonModuleConfig } from "./config/logging/winstonModule.config";
-import helmet from "@fastify/helmet";
 import { corsConfig } from "./config/sec/cors.config";
 
 async function bootstrap() {
@@ -17,8 +18,11 @@ async function bootstrap() {
 			logger: WinstonModule.createLogger(winstonModuleConfig),
 		},
 	);
-	app.register(helmet);
+
+	await app.register(helmet);
 	app.enableCors(corsConfig);
+	await app.register(fastifyCsrfProtection);
+
 	await app.listen(3000);
 }
 bootstrap();
